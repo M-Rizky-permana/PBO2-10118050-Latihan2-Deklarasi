f96da635253eb61c6889dd95b1eedb8dfbefb943
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 19:50:14 +0700

    Add files via upload

---
+++ PBO210118050Latihan2Deklarasi/src/PBO210118050Latihan2Deklarasi/PBO210118050Latihan2Deklarasi.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO210118050Latihan2Deklarasi;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program :Deklarasi & pengaksesan variabel 
+ */
+public class PBO210118050Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14;//konstanta
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    
+        
+    }
+    
+}
